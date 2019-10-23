39966829a62511d4bc12eaf03ab28b3393c40eca
Saurabh Singh <sausing@microsoft.com>
2019-10-23 15:05:07 -0700

    Repro in progress

--- connectionleak/Program.cs
+++ connectionleak/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.Data.SqlClient;
+using System.Threading.Tasks;
 
 namespace connectionleak
 {
@@ -13,17 +14,32 @@ namespace connectionleak
                 Console.WriteLine("Insufficient arguments: dotnet run <username> <pwd>");
                 return;
             }
-            string connectionString = $"Data Source=sql-fxtest; Initial Catalog = master; pwd={args[1]};user Id={args[0]}";
-            for(int i = 0; i < MAX_CONNECTION_COUNT; i++)
-            using(SqlConnection connection = new SqlConnection(connectionString)) {
-                connection.Open();
-                using(SqlCommand command = new SqlCommand("select 1", connection)) {
-                    var a =command.ExecuteScalar();
-                    Console.WriteLine(a);
-                }
+            Task<int>[] tasks = new Task<int>[MAX_CONNECTION_COUNT];
+            for ( int i = 0; i < MAX_CONNECTION_COUNT; i++)
+            {
+                tasks[i] = ExecuteScalar(args[0], args[1]);
             }
+            Task t  = Task.WhenAll(tasks);
+            t.Wait();
             Console.WriteLine($"Successfully open {MAX_CONNECTION_COUNT} SqlConnections");
             Console.ReadLine();
+        }
+
+        private static async Task<int> ExecuteScalar(string username, string password)
+        {
+            int result = -1;
+            string connectionString = $"Data Source=sql-fxtest; Initial Catalog = master; pwd={password};user Id={username}";
+            for (int i = 0; i < 2; i++)
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    await connection.OpenAsync();
+                    using (SqlCommand command = new SqlCommand("select 1", connection))
+                    {
+                        result = (int) await command.ExecuteScalarAsync();
+                        
+                    }
+                }
+            return result;
         }
     }
 }
