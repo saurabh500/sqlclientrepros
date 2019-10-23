64f1c0ce718547291308abc119d43432b17d8372
Saurabh Singh <sausing@microsoft.com>
2019-10-22 18:30:55 -0700

    adding error checks

--- connectionleak/Program.cs
+++ connectionleak/Program.cs
@@ -9,13 +9,17 @@ namespace connectionleak
 
         static void Main(string[] args)
         {
+            if(args.Length < 2) {
+                Console.WriteLine("Insufficient arguments: dotnet run <username> <pwd>");
+                return;
+            }
             string connectionString = $"Data Source=sql-fxtest; Initial Catalog = master; pwd={args[1]};user Id={args[0]}";
             for(int i = 0; i < MAX_CONNECTION_COUNT; i++)
             using(SqlConnection connection = new SqlConnection(connectionString)) {
                 connection.Open();
             }
             Console.WriteLine($"Successfully open {MAX_CONNECTION_COUNT} SqlConnections");
-
+            Console.ReadLine();
         }
     }
 }
