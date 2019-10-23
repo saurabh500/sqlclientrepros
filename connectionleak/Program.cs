845d03132ff519b25f0a8732a9afcf7292f27d4e
Saurabh Singh <sausing@microsoft.com>
2019-10-22 18:27:50 -0700

    Adding latest

--- connectionleak/Program.cs
+++ connectionleak/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using Microsoft.Data.SqlClient;
 
 namespace connectionleak
 {
     class Program
     {
+        static int MAX_CONNECTION_COUNT = 300;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string connectionString = $"Data Source=sql-fxtest; Initial Catalog = master; pwd={args[1]};user Id={args[0]}";
+            for(int i = 0; i < MAX_CONNECTION_COUNT; i++)
+            using(SqlConnection connection = new SqlConnection(connectionString)) {
+                connection.Open();
+            }
+            Console.WriteLine($"Successfully open {MAX_CONNECTION_COUNT} SqlConnections");
+
         }
     }
 }
